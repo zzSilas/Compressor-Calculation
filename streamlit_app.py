450e83dd061f9fe5c94d70e7224df1de28d75604
zzSilas <165250805+zzSilas@users.noreply.github.com>
2024-12-11 13:17:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("🎈 Compressor COP calculation")
 # 输入
 compressor_name = st.text_input("Compressor Name")
-T_evap = st.number_input("Evaporating Temperature (°C)", value=0.0)
-T_cond = st.number_input("Condensing Temperature (°C)", value=0.0)
-tSuc = st.number_input("Suction Temperature (°C)", value=0.0)
+T_evap = st.number_input("Evaporating Temperature (°C)", value=-25.0)
+T_cond = st.number_input("Condensing Temperature (°C)", value=35.0)
+tSuc = st.number_input("Suction Temperature (°C)", value=25.0)
 nCompressor = st.number_input("Compressor Efficiency (%)", value=100.0)
 # 上传Excel文件
 uploaded_file = st.file_uploader("Upload Messdata.xlsx", type=["xlsx"])
