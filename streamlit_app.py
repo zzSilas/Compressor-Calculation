6f5e8f4f19c519ff90b7b08af900ce9efe3075ba
zzSilas <165250805+zzSilas@users.noreply.github.com>
2024-12-11 11:16:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Compressor COP calculation")
+compressor_name = st.text_input("Compressor Name")
+T_evap = st.number_input("Evaporating Temperature (°C)", value=0.0)
+T_cond = st.number_input("Condensing Temperature (°C)", value=0.0)
+tSuc = st.number_input("Suction Temperature (°C)", value=0.0)
+nCompressor = st.number_input("Compressor Efficiency (%)", value=100.0)
